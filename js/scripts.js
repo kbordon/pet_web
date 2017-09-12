6e9633153b5fe0dfb42cb25f1c46e16f3309f40c
Kimberly Bordon and Rane Fields <kimberly@>
2017-09-12 10:50:16 -0700

    add togglable highlighting

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
   $("button#light").click(function() {
     $("body").removeClass();
-    $("body").addClass("light-background");
+    $("body").addClass("light-theme");
   });
   $("button#dark").click(function() {
     $("body").removeClass();
-    $("body").addClass("dark-background");
+    $("body").addClass("dark-theme");
   });
   $("button#neutral").click(function() {
     $("body").removeClass();
   });
 
   $(".highlightable").click(function() {
-    $(".highlightable").addClass("highlight");
+    if($(this).hasClass("highlight"))
+    {
+      $(this).removeClass("highlight");
+    }
+    else
+    {
+      $(".highlight").removeClass("highlight");
+      $(this).addClass("highlight");
+    }
   });
 });
